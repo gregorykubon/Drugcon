29d4e8c50a6356d15e9a81d2d0d0a413ee7a74b3
gregorykubon <ziombeltcl@o2.pl>
2017-05-09 01:48:26 +0200

    database model ver 0.8

--- app/src/main/java/com/example/grzegorz/drugcon/List.java
+++ app/src/main/java/com/example/grzegorz/drugcon/List.java
@@ -1,13 +1,20 @@
 package com.example.grzegorz.drugcon;
 
+import android.database.Cursor;
+import android.database.SQLException;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Toast;
+
+import java.io.IOException;
 
 public class List extends AppCompatActivity {
+
+    Cursor c = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +23,35 @@ public class List extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        DatabaseModel myDb = new DatabaseModel(List.this);
+        try {
+            myDb.createDataBase();
+        } catch (IOException ioe) {
+            throw new Error("Unable to create database");
+        }
+        try {
+            myDb.openDataBase();
+        } catch (SQLException sqle) {
+            throw sqle;
+        }
+        Toast.makeText(List.this, "Successfully Imported", Toast.LENGTH_SHORT).show();
+        c = myDb.query("drug", null, null, null, null, null, null);
+        if (c.moveToFirst()) {
+            do {
+                Toast.makeText(List.this,
+                        "_id: " + c.getString(0) + "\n" +
+                                "DATE: " + c.getString(1) + "\n" +
+                                "TIME: " + c.getString(2) + "\n" +
+                                "HEIGHT:  " + c.getString(3),
+                        Toast.LENGTH_LONG).show();
+            } while (c.moveToNext());
+        }
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "A tu po co?", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
