d849a6130e49b324d201f5111f81902fd14df886
gregorykubon <ziombeltcl@o2.pl>
2017-05-30 13:18:03 +0200

    login activity complete, optimization and bug tracking expect menu activity needs personalization search activity shows drug names and filters

--- app/src/main/java/com/example/grzegorz/drugcon/presentation/presenter/blank/SearchPresenter.java
+++ app/src/main/java/com/example/grzegorz/drugcon/presentation/presenter/blank/SearchPresenter.java
@@ -1,11 +1,50 @@
 package com.example.grzegorz.drugcon.presentation.presenter.blank;
 
 
+import android.database.Cursor;
+import android.database.SQLException;
+
+import com.example.grzegorz.drugcon.DataReader;
+import com.example.grzegorz.drugcon.DatabaseModel;
+import com.example.grzegorz.drugcon.LoginModel;
 import com.example.grzegorz.drugcon.presentation.view.blank.SearchView;
 import com.arellomobile.mvp.InjectViewState;
 import com.arellomobile.mvp.MvpPresenter;
 
+import java.io.IOException;
+
 @InjectViewState
 public class SearchPresenter extends MvpPresenter<SearchView> {
+
+    public String[] getAll(DataReader dr) {
+        Cursor c = null;
+        DatabaseModel myDb = new DatabaseModel(dr.getContext());
+        try {
+            myDb.createDataBase();
+        } catch (IOException ioe) {
+            throw new Error("Unable to create database");
+        }
+        try {
+            myDb.openDataBase();
+        } catch (SQLException sqle) {
+            throw sqle;
+        }
+
+        c = myDb.query("drug",null,null,null,null,null,null);
+        String products[] = new String[c.getCount()];
+        c.moveToFirst();
+        int i=0;
+        do{
+            products[i]=c.getString(c.getColumnIndex("name"));
+            i++;
+        }while(c.moveToNext());
+
+        c.close();
+        myDb.close();
+
+        return products;
+
+    }
+
 
 }
--- app/src/main/java/com/example/grzegorz/drugcon/ui/activity/blank/Search.java
+++ app/src/main/java/com/example/grzegorz/drugcon/ui/activity/blank/Search.java
@@ -3,7 +3,13 @@ package com.example.grzegorz.drugcon.ui.activity.blank;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
 
+import com.example.grzegorz.drugcon.DataReader;
 import com.example.grzegorz.drugcon.R;
 import com.example.grzegorz.drugcon.presentation.view.blank.SearchView;
 import com.example.grzegorz.drugcon.presentation.presenter.blank.SearchPresenter;
@@ -12,6 +18,9 @@ import com.arellomobile.mvp.MvpActivity;
 
 
 import com.arellomobile.mvp.presenter.InjectPresenter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Search extends MvpActivity implements SearchView {
     public static final String TAG = "Search";
@@ -24,10 +33,59 @@ public class Search extends MvpActivity implements SearchView {
         return intent;
     }
 
+    // List view
+    private ListView lv;
+
+    // Listview Adapter
+    ArrayAdapter<String> adapter;
+
+    // Search EditText
+    EditText inputSearch;
+
+
+    // ArrayList for Listview
+    ArrayList<HashMap<String, String>> productList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        final DataReader dr = new DataReader(this);
+
+        String products[] = mSearchPresenter.getAll(dr);
+
+
+        lv = (ListView) findViewById(R.id.list_view);
+        inputSearch = (EditText) findViewById(R.id.inputSearch);
+
+        // Adding items to listview
+        adapter = new ArrayAdapter<String>(this, R.layout.content_list, R.id.product_name, products);
+        lv.setAdapter(adapter);
+
+        /**
+         * Enabling Search Filter
+         * */
+        inputSearch.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void onTextChanged(CharSequence cs, int arg1, int arg2, int arg3) {
+                // When user changed the Text
+                Search.this.adapter.getFilter().filter(cs);
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence arg0, int arg1, int arg2,
+                                          int arg3) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable arg0) {
+                // TODO Auto-generated method stub
+            }
+        });
     }
 }
